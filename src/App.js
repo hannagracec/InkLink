faeaff769b9e2362c9ef0661bf0063798b6480fc
hannagracec <hannacho847@gmail.com>
2024-02-17 12:21:21 -0700

    deleted switch

--- src/App.js
+++ src/App.js
@@ -5,9 +5,6 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route/>
-      </Switch>
     </Router>
   );
 }
