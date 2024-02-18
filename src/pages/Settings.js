9496b5eb64b30b6e2fd8abbabe96e7b94c595283
Sarah Y <sarahyip64@gmail.com>
2024-02-17 18:56:49 -0700

    finish adding pfp to availble ones to choose from

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,3 +1,10 @@
+import Man1 from "../pages/FriendsPP/Man1.png";
+import Man2 from "../pages/FriendsPP/Man2.png";
+import Man3 from "../pages/FriendsPP/Man3.png";
+import Woman1 from "../pages/FriendsPP/Woman1.png";
+import Woman2 from "../pages/FriendsPP/Woman2.png";
+import Woman3 from "../pages/FriendsPP/Woman3.png";
+import Woman4 from "../pages/FriendsPP/Woman4.png";
 import settingsicon from '../pages/SettingsImages/settingsicon.png';
 import React, { useEffect, useState } from 'react';
 import './Settings.css';
@@ -37,26 +44,28 @@ export default function Settings() {
     return (
         <div>
             <body>
-                <table>
-                    <tr>
-                        <th><h1 className="header">Settings</h1></th>
-                        <div style={{ position: 'absolute', top: 25, right: 30 }}>
-                            <img src={settingsicon} alt="settings icon" width="40px" />
-                        </div>
-                    </tr>
-                </table>
+                <div className="top">
+                    <table>
+                        <tr>
+                            <th><h1 className="header">Settings</h1></th>
+                            <div style={{ position: 'absolute', top: 25, right: 30 }}>
+                                <img src={settingsicon} alt="settings icon" width="40px" />
+                            </div>
+                        </tr>
+                    </table>
+                </div>
                 <div>
                     <button type="button" className="collapsible">
                         Change Profile Picture
                     </button>
                     <div className="content">
-                        <div className ="profile-options" border="none">
-                            <button><img src="" alt ="pfp 1"></img></button>
-                            <button><img src="" alt ="pfp 2"></img></button>
-                            <button><img src="" alt ="pfp 3"></img></button>
-                            <button><img src="" alt ="pfp 4"></img></button>
-                            <button><img src="" alt ="pfp 5"></img></button>
-                            <button><img src="" alt ="pfp 6"></img></button>
+                        <div className ="profile-options">
+                            <button><img src={Man1} alt ="pfp 1"></img></button>
+                            <button><img src={Man2} alt ="pfp 2"></img></button>
+                            <button><img src={Man3} alt ="pfp 3"></img></button>
+                            <button><img src={Woman1} alt ="pfp 4"></img></button>
+                            <button><img src={Woman1} alt ="pfp 5"></img></button>
+                            <button><img src={Woman3} alt ="pfp 6"></img></button>
                         </div>
                     </div>
 
